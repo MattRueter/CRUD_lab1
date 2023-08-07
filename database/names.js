947e1031d6da16a5ab8939f1bf45dbb637370b51
Robert Matthew Rueter <84277551+MattRueter@users.noreply.github.com>
2023-08-07 09:26:17 -0500

    add more properties to db name items.

--- database/names.js
+++ database/names.js
@@ -1,9 +1,9 @@
 const express =require("express");
 
 const names =[
-    {id:1, name: "Matt"},
-    {id:2, name: "Victoria"},
-    {id:3, name: "Michael"}
+    {id:1, name: "Matt", dob: "24/10/1980", nationality: "U.S.A"},
+    {id:2, name: "Victoria", dob: "01/31/1976", nationality: "British"},
+    {id:3, name: "Michael", dob: "27/12/1973", nationality: "U.S.A"}
 ]
 
 module.exports = names;
